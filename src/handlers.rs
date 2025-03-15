56d447cb2e50d94aed81ac9e52e899da0a718773
garvbox <garv@garvbox.net>
2025-03-15 17:26:24 +0000

    Implement rudimentary vehicle list rendering
    Very simple html list page to demonstrate listing vehicles from the
    database. Tried with manually instered vehicles and its working very
    well

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,9 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse, response::Json};
+use askama::Template;
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{Html, IntoResponse},
+};
 use deadpool_diesel::postgres::Pool;
 use diesel::prelude::*;
 
@@ -8,9 +13,13 @@ pub async fn not_found(uri: axum::http::Uri) -> impl IntoResponse {
     (StatusCode::NOT_FOUND, format!("No route {}", uri))
 }
 
-pub async fn list_vehicles(
-    State(pool): State<Pool>,
-) -> Result<Json<Vec<models::Vehicle>>, (StatusCode, String)> {
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate<'a> {
+    vehicles: &'a Vec<models::Vehicle>,
+}
+
+pub async fn list_vehicles(State(pool): State<Pool>) -> Result<Html<String>, (StatusCode, String)> {
     let conn = pool.get().await.map_err(internal_error)?;
     let res = conn
         .interact(|conn| {
@@ -21,7 +30,9 @@ pub async fn list_vehicles(
         .await
         .map_err(internal_error)?
         .map_err(internal_error)?;
-    Ok(Json(res))
+
+    let template = IndexTemplate { vehicles: &res };
+    Ok(Html(template.render().unwrap()))
 }
 
 /// Utility function for mapping any error into a `500 Internal Server Error`
